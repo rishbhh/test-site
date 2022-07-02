c10f69330dc69a7af73fb45a283b296e114ca28e
rishbhh <sevikash04@gmail.com>
2022-07-02 14:11:15 +0530

    update

--- assets/js/shortcodes.js
+++ assets/js/shortcodes.js
@@ -44,31 +44,6 @@
       }
     };
 
-    var flatCounter = function () {
-      if ($(document.body).hasClass("counter-scroll")) {
-        var a = 0;
-        $(window).scroll(function () {
-          var oTop = $(".box").offset().top - window.innerHeight;
-          if (a == 0 && $(window).scrollTop() > oTop) {
-            if ($().countTo) {
-              $(".box")
-                .find(".number")
-                .each(function () {
-                  var to = $(this).data("to"),
-                    speed = $(this).data("speed");
-  
-                  $(this).countTo({
-                    to: to,
-                    speed: speed,
-                  });
-                });
-            }
-            a = 1;
-          }
-        });
-      }
-    };
-  
     var popupGallery = function () {
       if ($().magnificPopup) {
         $(".popup-gallery").magnificPopup({
@@ -147,7 +122,6 @@
 
   
     $(function () {
-      flatCounter();
       popupGallery();
       popupVideo();
       btnQuantity();
